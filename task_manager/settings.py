39fb484d594e446416024ead28eb189c07ab80f6
Andres <totesanders@gmail.com>
2026-01-11 16:25:24 -0800

    Settings changed for railway compatabilty

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -65,13 +65,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'task_manager.wsgi.application'
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL', default=f'postgresql://postgres:CrabLegend@localhost:5432/taskdb'),
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        conn_health_checks=True,
+        ssl_require=True,
     )
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
